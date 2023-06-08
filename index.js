d11244f439bb2f047925df0efe045776eadc79a9
dimka3553 <37512400+dimka3553@users.noreply.github.com>
2023-06-08 23:43:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("ggggggg World!");
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+app.listen(8080, () => {
+  console.log("Example app listening on port 8080!");
 });
